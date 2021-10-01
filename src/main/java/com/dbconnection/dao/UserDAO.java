3f7099247bcf80f2c1b3c3e1f6643ecfdf34c23b
pao_cuellar <pao.cuellar18@hotmail.com>
2021-10-01 14:14:45 -0500

    DAO LogIn

--- src/main/java/com/dbconnection/dao/UserDAO.java
+++ src/main/java/com/dbconnection/dao/UserDAO.java
@@ -42,22 +42,16 @@ public class UserDAO {
         int rowsAffectted = 0;
         try {
             con = DbConnection.getConnection();
-            // En el proyecto solo podran hacer uso de Store Procedures
-            // No llamadas directas como esta
-            // Esta linea prepara la llamada a la base de datos para insertar
-            // Cada ? significa un valor a ser remplazado
-            //statement = con.prepareCall("INSERT INTO user (nameUser, dobUser, emailUser, passUser, nickUser, imageUser) VALUES (?,?,?,?,?,?)");
+            
             statement = con.prepareCall("call InsertUser(?,?,?,?,?,?)");
-            // Remplazamos el primer parametro por el nombre
+            
             statement.setString(1, user.getUserName());
             statement.setString(2, user.getUserdob());
             statement.setString(3, user.getUserEmail());
             statement.setString(4, user.getPassword());
             statement.setString(5, user.getNickname());
-            // El ultimo por la url de la imagen
             statement.setString(6, user.getUrlImage());
-            // Ejecuta el Statement y retorna cuantos registros
-            // fueron actualizados
+            
             
             rowsAffectted = statement.executeUpdate();
         } catch (SQLException ex) {
@@ -71,14 +65,15 @@ public class UserDAO {
     }
     
     public static UserModel logInUser(UserModel user) throws Exception {
-    	Connection con = null;
-    	CallableStatement statement = null;
+    	//Connection con = null;
+    	//CallableStatement statement = null;
 
         //boolean rowsAffectted = false;
         try{
-            con = DbConnection.getConnection();
+            Connection con = DbConnection.getConnection();
 
-            statement = con.prepareCall("call LogInUser(?,?)");
+            
+            CallableStatement statement = con.prepareCall("call LogInUser(?,?)");
             statement.setString(1, user.getNickname());
             statement.setString(2, user.getPassword());
             ResultSet resultSet = statement.executeQuery();
@@ -101,9 +96,6 @@ public class UserDAO {
         }catch (SQLException ex) {
             System.out.println(ex.getMessage());
             //rowsAffectted = false;
-        } finally {
-        	statement.close();
-            con.close();
         }
        
         return null;
