42fbb73bd94ec077f7a845ce13f59683e726eca4
pao_cuellar <pao.cuellar18@hotmail.com>
2021-10-10 18:28:45 -0500

    Se borraron comentarios

--- src/main/java/com/dbconnection/dao/UserDAO.java
+++ src/main/java/com/dbconnection/dao/UserDAO.java
@@ -11,30 +11,11 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- *
- * @author magoc
- */
+
 public class UserDAO {
 
-    // Query de creacion de Tabla
-    /**
-     * CREATE TABLE `user` ( `idUser` INT NOT NULL AUTO_INCREMENT, `nameUser`
-     * VARCHAR(45) NULL, `password` VARCHAR(45) NULL, `urlImage` VARCHAR(255)
-     * NULL, PRIMARY KEY (`idUser`));
-     *
-     */
-    /**
-     * Inserta un usuario en la base de datos
-     *
-     * @param user
-     * @return
-     */
-
-     //TODO: Es ideal incluir el "throws Exception" ?
+  
     public static int insertUser(UserModel user) throws Exception {
     	Connection con = null;
     	CallableStatement statement = null;
@@ -77,10 +58,9 @@ public class UserDAO {
             statement.setString(1, user.getNickname());
             statement.setString(2, user.getPassword());
             ResultSet resultSet = statement.executeQuery();
-            // Si el resultSet tiene resultados lo recorremos
+
             while (resultSet.next()) {
-                // Obtenemos el valor del result set en base al nombre de la
-                // columna
+
                 int id=resultSet.getInt("ID");
                 String userName = resultSet.getString("Nombre");
                 String userdob = resultSet.getString("Fecha_de_nacimiento");
@@ -101,36 +81,4 @@ public class UserDAO {
         return null;
     }
 
-    
-    public static List<UserModel> getUsers() throws Exception  {
-        List<UserModel> users = new ArrayList<>();
-        Connection con = null;
-        CallableStatement statement = null;
-
-        try {
-            con = DbConnection.getConnection();
-            statement = con.prepareCall("SELECT * FROM user");
-            ResultSet resultSet = statement.executeQuery();
-            // Si el resultSet tiene resultados lo recorremos
-            while (resultSet.next()) {
-                // Obtenemos el valor del result set en base al nombre de la
-                // columna
-                String name = resultSet.getString("nameUser");
-                String dob = resultSet.getString("dobUser");
-                String email = resultSet.getString("emailUser");
-                String password = resultSet.getString("passUser");
-                String nickname = resultSet.getString("nickUser");
-                String urlImage = resultSet.getString("imageUser");
-                // Agregamos el usuario a la lista
-                users.add(new UserModel(name, dob, email, password, nickname,urlImage));
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        } finally {
-            statement.close();
-            con.close();
-        }
-
-        return users;
-    }
 }
