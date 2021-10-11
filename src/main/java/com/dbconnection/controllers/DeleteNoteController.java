148ab416d1601e613d27b4f42a83006ad60188f4
pao_cuellar <pao.cuellar18@hotmail.com>
2021-10-10 19:23:23 -0500

    Servlet borrar nota

---
+++ src/main/java/com/dbconnection/controllers/DeleteNoteController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dbconnection.controllers;
+
+import com.dbconnection.dao.NoteDAO;
+import com.dbconnection.models.NoteModel;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "DeleteNoteController", urlPatterns = {"/DeleteNoteController"})
+public class DeleteNoteController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String idNoteS = request.getParameter("idNote");
+        int idNote=Integer.parseInt(idNoteS);
+        NoteModel note = new NoteModel(idNote);
+        
+            try {
+		NoteModel result=NoteDAO.deleteNote(note);
+                        if (result!=null){
+
+                            request.getRequestDispatcher("dashboard.jsp").forward(request,response);
+                        }else {
+                            response.sendRedirect("fail.jsp");
+                        }
+		} catch (Exception e) {
+                        //response.sendRedirect("fail.jsp"); 
+			e.printStackTrace();
+		}
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
